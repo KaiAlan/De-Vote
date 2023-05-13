f43af89dc173fbc5e2b19c356e425e1bae37ee32
Kaialan <satyajit1501@gmail.com>
2023-05-13 19:21:06 +0530

    🚧 Frontend base setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex flex-col justify-center items-center p-36'>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+      <h2>Hmeloo</h2>
+    </div>
+  )
+}
+
+export default App
